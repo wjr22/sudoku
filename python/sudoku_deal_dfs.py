da18151cecdea713c0a0da4c7615c30679095b31
Wang jiaruo <wangjiaruo22@hotmail.com>
2019-04-18 11:42:38 +0800

    Upgrade organizational form by language

---
+++ python/sudoku_deal_dfs.py
@@ -0,0 +1,36 @@
+#! python
+#encoding:utf-8
+from python import tool
+
+
+def dfs(sudoku, x, y):
+    """
+    暴力推解法
+    :param sudoku:
+    :param x: 横坐标 以0开头，递增
+    :param y: 纵坐标 同理
+    :return:
+    """
+    if tool.is_finish(sudoku):
+        print(sudoku)
+        return
+    else:
+        for i in range(x,9):    #y先不变(range 左闭右开)
+            if sudoku[i][y] == 0:  # 有了跳过
+                # 跳过  事实证明这样跳不了
+                # dfs(sudoku,x+1,y+1)
+                for k in range(1, 10):
+                    if tool.judge(sudoku, i, y, k):
+                        sudoku[i][y] = k
+                        if y == 8:
+                            dfs(sudoku, 0, y + i + 1)
+                        else:
+                            dfs(sudoku, y + 1, i)
+                        sudoku[i][y] = 0
+
+
+
+
+
+
+
